9675f0c23e48249d3ec79d58e0abe359bf88df9c
Lucas Brito <eu.lucasbrittoo@gmail.com>
2026-09-22 23:22:42 -0300

    feat: exibe dados do vinho no console

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,13 @@ var anoAtual = 2026
 var idade = anoAtual - safra
 
 alert("Cadastro realizado! Veja os detalhes no console.")
+
+// mostrando no console
+alert("A seguir, veja os detalhes do vinho no console.")
+console.log("===== VINHO CADASTRADO =====")
+console.log("Nome: " + nome)
+console.log("Tipo: " + tipo)
+console.log("Safra: " + safra)
+console.log("Idade do vinho: " + idade + " anos")
+console.log("Quantidade em estoque: " + quantidade + " unidades")
+console.log("============================")
